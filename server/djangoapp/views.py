da29aa8bbba7f4043f2f926ace04484557cdec7d
Jeremy Bosse <134167755+JeremyBBosse1@users.noreply.github.com>
2025-04-02 16:11:31 -0400

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -129,7 +129,7 @@ def get_dealer_details(request, dealer_id):
 
 
 # Create a `add_review` view to submit a review
-def add_review(request):
+def add_review(request, response):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
